5972c5fe964521670530e3daa7c67b0eff656956
Goverdhan Koyalkar <Goverdhan Koyalkar>
2015-12-24 00:32:51 +0530

    fixed nav

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.run(['$rootScope', '$state', '$stateParams', function (rootScope, state, sta
       logo:"",
       branches:[
             {branchId:"1001", location:{address:"6th Floor, Quadrant No: 3, Cyber Towers, Hitec City", city:"Hyderabad", state:"Andhra Pradesh", pin:"", country:"India"}, mobile:"091-9885514982", phone:"040-24601181", email:"info@shardhasoftware.com" },
-            {branchId:"1002", location:{address:"38725 Lexington Street", city:"Fremont", state:"California", pin:"94538", country:"US"}, mobile:"", phone:"605-218-5163", email:"info@shardhasoftware.com" },
-            {branchId:"1003", location:{address:"", city:"Seattle", state:"Washington", pin:"", country:"US"}, mobile:"", phone:"425-270-5178", email:"info@shardhasoftware.com" }
+            {branchId:"1002", location:{address:"38725 Lexington Street", city:"Fremont", state:"California", pin:"94538", country:"US"}, mobile:"605-218-5163", phone:"605-218-5163", email:"info@shardhasoftware.com" },
+            {branchId:"1003", location:{address:"", city:"Seattle", state:"Washington", pin:"", country:"US"}, mobile:"425-270-5178", phone:"425-270-5178", email:"info@shardhasoftware.com" }
             ]
     };
 
